c04ee3b64f9797e5f857cc240ef0c14d37b82b34
JaeJeong <hjj19911@naver.com>
2019-12-06 18:01:13 +0900

    REST API 적용 게시판

--- src/main/java/board/BoardApplication.java
+++ src/main/java/board/BoardApplication.java
@@ -3,6 +3,8 @@ package board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication(exclude= {MultipartAutoConfiguration.class})
 public class BoardApplication {
@@ -10,5 +12,15 @@ public class BoardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
+	
+    /**
+     * HiddenHttpMethodFilter  
+     */
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+        return filter;
+    }
+
 
 }
